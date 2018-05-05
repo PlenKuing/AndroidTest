9518c21dcbbf5b65beb59bb70f4cac00ae51b0e3
PlenKuing <352844752@qq.com>
2018-05-06 01:16:45 +0800

    完成了帧布局的内容

--- app/src/main/java/com/example/plenkuing/androidtest/FrameActivity.java
+++ app/src/main/java/com/example/plenkuing/androidtest/FrameActivity.java
@@ -2,21 +2,52 @@ package com.example.plenkuing.androidtest;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.nfc.cardemulation.HostNfcFService;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.annotation.Nullable;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 /**
  * Created by PlenKuing on 2018/4/21.
  */
 
 public class FrameActivity extends Activity {
+    private  int currentPhoto = 0;
+    private ImageView iv;
+
+
+    private int[] photos = new int[]{
+            R.drawable.first,
+            R.drawable.second,
+            R.drawable.thrid,
+            R.drawable.fouth,
+            R.drawable.fifth
+    };
+
+    Handler handler = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            if(msg.what==0x123){
+                currentPhoto++;
+                iv.setImageResource(photos[currentPhoto%5]);
+            }
+            super.handleMessage(msg);
+        }
+    };
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.frame);
         Button returnBt = findViewById(R.id.returnBt);
+        iv = findViewById(R.id.iv);
+
 
         returnBt.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -26,5 +57,12 @@ public class FrameActivity extends Activity {
                 finish();//关闭
             }
         });
+
+        new Timer().schedule(new TimerTask() {
+            @Override
+            public void run() {
+                handler.sendEmptyMessage(0x123);
+            }
+        },0,1500);
     }
 }
